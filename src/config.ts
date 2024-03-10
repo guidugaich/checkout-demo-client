6160a2967dbcf53da6b2f0b1e4a95e1cdd27d60a
guidugaich <guidugaich.dev@gmail.com>
2024-03-10 17:25:28 -0300

    adjust live path

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ const config = {
   checkoutPublicKey: import.meta.env.VITE_CHECKOUT_PUBLIC_KEY,
   serverSideBaseUrl: {
     local: 'http://localhost:3000',
-    live: 'https://checkout-demo-server.onrender.com/'
+    live: 'https://checkout-demo-server.onrender.com'
   }
 }
 
