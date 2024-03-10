de47481aa277ce274c0cb5bdacb7709707c825f7
guidugaich <guidugaich.dev@gmail.com>
2024-03-10 17:06:37 -0300

    using render deployment

--- src/services/api/payments.ts
+++ src/services/api/payments.ts
@@ -7,6 +7,20 @@ export type RequestPaymentSessionParams = {
 }
 
 export async function requestPaymentSession(params: RequestPaymentSessionParams) {
+  const url = `${config.serverSideBaseUrl.live}/request-payment-session`;
+  const paymentSession = await fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(params)
+  })
+    .then((res) => res.json()
+    .then(data =>  data)
+  );
+
+  return paymentSession;
+};
+
+export async function requestPaymentComponent(params: RequestPaymentSessionParams) {
   const url = `${config.serverSideBaseUrl.local}/request-payment-session`;
   const paymentSession = await fetch(url, {
     method: 'POST',
