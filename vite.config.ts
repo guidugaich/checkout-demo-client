5f05683e630cd910f42e64d77b288bde1ca65e23
guidugaich <guidugaich.dev@gmail.com>
2024-03-11 19:10:30 -0300

    back to normal

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/checkout-demo-client",
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        404: "public/404.html",
-      },
-    },
-  },
 })
