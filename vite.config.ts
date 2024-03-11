ea3aa9b216e156f444f4750b131ff7cc5bc9f48b
guidugaich <guidugaich.dev@gmail.com>
2024-03-11 19:04:39 -0300

    fix 404 for routes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/checkout-demo-client"
+  base: "/checkout-demo-client",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        404: "public/404.html",
+      },
+    },
+  },
 })
