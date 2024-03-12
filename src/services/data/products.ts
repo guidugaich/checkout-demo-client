68a97c944fb5088109fc66623b36fba190999d34
guidugaich <guidugaich.dev@gmail.com>
2024-03-12 01:57:52 -0300

    fix img paths

--- src/services/data/products.ts
+++ src/services/data/products.ts
@@ -1,7 +1,7 @@
 import { IProduct } from "../../components/ProductList"
 
 const products: IProduct[] = [
-    { id: '50dcfaa5-1ca3-414d-a1cf-286a86f28a4a', name: 'White T-Shirt', price: 15, imgSrc: '.public/white.jpeg' },
+    { id: '50dcfaa5-1ca3-414d-a1cf-286a86f28a4a', name: 'White T-Shirt', price: 15, imgSrc: './public/white.jpeg' },
     { id: '166e4b39-284f-43cf-aca7-dd36adfd8c1a', name: 'Black T-Shirt', price: 15, imgSrc: './public/black.jpeg' },
     { id: '33528b18-6c69-4d4b-bca7-59cff1b06231', name: 'Red T-Shirt', price: 30, imgSrc: './public/red.jpeg' },
     { id: 'b8068bed-c7aa-4c65-b8cc-21f09ca97ecd', name: 'Blue T-Shirt', price: 40, imgSrc: './public/blue.jpeg' },
