dd43946bf2b87947c5a939840133f3671a293203
guidugaich <guidugaich.dev@gmail.com>
2024-03-11 22:56:01 -0300

    add link to success page

--- src/pages/PaymentSuccessPage.tsx
+++ src/pages/PaymentSuccessPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PaymentSuccessPage: React.FC = () => {
   return (
-    <header>
-      <h1>Your payment was successful</h1>
-    </header>
+    <>
+        <header>
+            <h1>Your payment was successful</h1>
+        </header>
+        <Link to={'/'}>Back to the Store</Link>
+    </>
   );
 };
 
